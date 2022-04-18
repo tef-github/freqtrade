3d58fb271ac4e10b01d0dab15ed466b8e38d35db
root <root@14303.example.com>
2022-04-18 20:11:33 +0200

    indpedently running the backtest

--- romeo_ai_optimizer.py
+++ romeo_ai_optimizer.py
@@ -90,17 +90,18 @@ class Romeo_ai_optimizer:
             #         # Change the parameters in brain_config2022-04-12 16:30:48,856 - freqtrade - ERROR - Incorrect syntax for 2022-04-12 16:30:48,856 - freqtrade - ERROR - Incorrect syntax for timerange "2020101-2020201"timerange "2020101-2020201"
             #         self.change_parameters_in_config(dup,dup_max_counter)
             #         # download the data
-            print(time.time())
+            print("Starting time: ",time.time())
             commands = """
             freqtrade download-data --config """+strategy["config"]+""" -t 5m --timerange """+str(year)+self.get_month_string(int(month))+"""01-"""+str(year)+self.get_month_string(int(month)+1)+"""01;
             python3 wao/freq_data_cleaner.py user_data/data/binance/BTC_USDT-5m.json;
             cd ../execution/;
             python3 month_data_download.py BTC """+self.get_month_string(int(month)-1)+""" """+str(year)+""";
             cd ../freqtrade;
-            screen -L -Logfile Logfile-No-optimizer-BTC-Strategy002-Backtest-20:40:00-April-16-2022.txt freqtrade backtesting -c """+strategy["config"]+""" -s """+strategy["strategy"]+""";
+            screen -L -Logfile Logfile-No-optimizer-BTC-Strategy002-Backtest-23:36:00-April-17-2022.txt freqtrade backtesting -c """+strategy["config"]+""" -s """+strategy["strategy"]+""";
             """
             print(commands)
-            os.system(commands)
+            # os.system(commands)
+            print("Finishing time: ",time.time())
                     # cumulative_profit = self.read_cumulative_profit_from_file()
                     # if(self.best_combination[2]<cumulative_profit):
                     #     self.best_combination[0] = dup
