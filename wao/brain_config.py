d8ce30860e623194f6cf64a668d81c55ed94833d
root <root@14303.example.com>
2022-04-22 10:59:02 +0200

    modifications

--- wao/brain_config.py
+++ wao/brain_config.py
@@ -1,9 +1,9 @@
 class BrainConfig:
 
-    BACKTEST_DUP = 0.30000000000000004
+    BACKTEST_DUP = 0.45
     BACKTEST_MAX_COUNT_DUP = 2
     BACKTEST_DOWNLOADED_JSON_DATA_FILE_PATH = ""
-    BACKTEST_COIN = 'BTC'
+    BACKTEST_COIN = 'ADA'
     BACKTEST_MONTH_LIST = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
     BACKTEST_DATA_CLEANER_YEAR = 2021
     BACKTEST_DATA_CLEANER_MONTH_INDEX = 0
@@ -11,5 +11,5 @@ class BrainConfig:
     WORKSPACE_PATH = "workspace2" if IS_BACKTEST else "workspace"
     _429_DIRECTORY = "/root/" + WORKSPACE_PATH + "/freqtrade/_429_directory/"
     IS_PARALLEL_EXECUTION = True
-    BACKTEST_THROTTLE_SECOND = 1
+    BACKTEST_THROTTLE_SECOND = 0.001
     MODE = "test"
